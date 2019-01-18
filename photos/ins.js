436d4b0a442c9eceb27fc526889b0eb85cf00517
王浩 <haowang162@creditease.cn>
2019-01-18 14:08:10 +0800

    Site updated: 2019-01-18 14:08:10

--- photos/ins.js
+++ photos/ins.js
@@ -116,8 +116,8 @@
         var data = res.list[j].arr;
         var liTmpl = "";
         for (var i = 0, len = data.link.length; i < len; i++) {
-          var minSrc = 'https://github.com/wanghaoListening/blog_photos/tree/master/min_photos' + data.link[i];
-          var src = 'https://github.com/wanghaoListening/blog_photos/tree/master/photos' + data.link[i];
+          var minSrc = 'https://github.com/wanghaoListening/blog_photos/tree/master/min_photos/' + data.link[i];
+          var src = 'https://github.com/wanghaoListening/blog_photos/tree/master/photos/' + data.link[i];
           var type = data.type[i];
           var target = src + (type === 'video' ? '.mp4' : '.jpg');
           src += '';
